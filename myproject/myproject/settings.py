51709f237ac58c33a100df6645af9c3dd3dfa5e9
Winni <winnie1238888@gmail.com>
2025-10-31 04:28:55 -0400

    remove api

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyBzFRAc8gyWXfdJQoYMq8OVf49pxFbAX2M")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-%_+43bn+)z5jf!hfawtugjt=6(8c@uxd(g3*dj%fk5#e)(28w'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
